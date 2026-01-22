353155f3dd94a7065e2cace518fb324e533833d8
TheNel13 <lattimer.nw@gmail.com>
2026-01-22 19:58:04 +0000

    Add case study answers for DS lifecycle

--- ML1_inclass.py
+++ ML1_inclass.py
@@ -30,7 +30,11 @@ iris.head()
 
 # %%
 # What mental models can we see from these data sets?
+# A mental model that we can see from this data set is model where physical measurements of flowers (sepal length, sepal width, petal length, and petal width) are used to distinguish between different species
 # What data science questions can we ask?
+# Can we accurately predict the species of an iris flower using its physical measurements?
+# Which features (sepal or petal dimensions) are most important for classification?
+# How well do different machine learning models perform on this dataset?
 
 # %%
 """
@@ -67,6 +71,7 @@ Patterns in data
 """
 # Look at the following tables: do you see any patterns? How could a
 # classification model point these out?
+# Yes, there are clear patterns in the tables, especially when the data are grouped by species. Petal measurements differ a lot between species, while sepal measurements overlap more. A classification model can pick up on these patterns by grouping similar flowers together and using those similarities to predict the species of new flowers.
 patterns = iris.groupby(['Species'])
 patterns['Sepal.Length'].describe()
 
@@ -94,9 +99,13 @@ Mild disclaimer
 # For now, ask yourself:
 
 # 1. What is the purpose of data splitting?
+# To separate data into training and evaluation sets so the model can be tested on unseen data and avoid overfitting.
 # 2. What can we learn from data testing/validation?
+# To measure how well the model generalizes and to tune parameters without biasing performance.
 # 3. How do we know if a model is working?
+# A model is working if it performs well on validation/testing data using appropriate metrics and shows consistent results.
 # 4. How could we find the model error?
+# Model error can be found by comparing predictions to true values using error metrics.
 
 # If you want, try changing the size of the test data
 # or the number of n_neighbors and see what changes!
